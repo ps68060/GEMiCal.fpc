26edab8e13a3f02b3d0949cf0ac91577c4e018e0
ps68060 <pslegg@scubadivers.co.uk>
2021-12-20 22:02:20 +0000

    Add error handling.

--- config.pas
+++ config.pas
@@ -28,15 +28,16 @@ uses
   Token;
 
   const
+    nameTk       = 'name';
     latTk        = 'lat';
     lngTk        = 'long';
     UTCoffsetTk  = 'UTCoffset';
 
   constructor TConfig.init;
   begin
-    name := 'default';
-    lat  := 51.4779;
-    lng  := 0.0;
+    name      := 'default';
+    lat       := 51.4779;
+    lng       := 0.0;
     UTCoffset := 0.0;
   end;
 
@@ -57,6 +58,8 @@ uses
     keyValue     : PToken;
     code         : Integer;
 
+    valReal      : Real;
+
   begin
     new(logger);
     logger^.init;
@@ -76,38 +79,74 @@ uses
 
       keyValue^.tokeniseInf(currentLn);
 
+      (* Get the name *)
+      if ( pos(nameTk, currentLn) = 1 )
+      then
+        name := keyValue^.part[2];
+
+
+      (* Get the latitude, if it is invalid, keep default *)
       if ( pos(latTk, currentLn) = 1 )
       then
-        val(keyValue^.part[2], lat, code);
+      begin
+        val(keyValue^.part[2], valReal, code);
 
-      if (code <> 0)
-      then
-        writeln ('Real conversion error of lat');
+        if (code <> 0)
+        then
+          writeln ('Real conversion error of lat');
+
+        if (abs(valReal) > 90.0)
+        then
+          logger^.log(INFO, 'lat invalid, check gemical.cnf')
+        else
+          lat := valReal;
+
+      end;
 
 
+      (* Get the longitude, if it is invalid, keep default *)
       if ( pos(lngTk, currentLn) = 1 )
       then
-        val(keyValue^.part[2], lng, code);
+      begin
+        val(keyValue^.part[2], valReal, code);
 
-      if (code <> 0)
-      then
-        writeln ('Real conversion error of lng');
+        if (code <> 0)
+        then
+          writeln ('Real conversion error of lng');
 
+        if (abs(valReal) > 180.0)
+        then
+          logger^.log(INFO, 'long invalid, check gemical.cnf')
+        else
+          lng := valReal;
 
+      end;
+
+      (* Get the UTC offset, if it is invalid, keep default *)
       if ( pos(UTCoffsetTk, currentLn) = 1 )
       then
-        val(keyValue^.part[2], UTCoffset, code);
+      begin
+        val(keyValue^.part[2], valReal, code);
 
-      if (code <> 0)
-      then
-        writeln ('Real conversion error of UTCoffset');
+        if (code <> 0)
+        then
+          writeln ('Real conversion error of UTCoffset');
+
+        if (abs(valReal) > 12.0)
+        then
+          logger^.log(INFO, 'UTCoffset invalid, check gemical.cnf')
+        else
+          UTCoffset := valReal;
+
+      end;
 
       Dispose (keyValue, Done);
     end;  (* while *)
 
+    logger^.log(DEBUG, 'location = ' + name);
     logger^.logReal(DEBUG,'lat = ', lat);
     logger^.logReal(DEBUG,'lng = ', lng);
-    logger^.logReal(DEBUG,'UTC ', UTCoffset);
+    logger^.logReal(DEBUG,'UTC ',   UTCoffset);
 
     close(cnfFile);
     Dispose (logger, Done);
