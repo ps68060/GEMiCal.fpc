9528767ae4de543264ba5390a08be3f33227c301
pslegg <pslegg@sis.tv>
2026-03-06 16:54:15 +0000

    Start adding unit tests.

--- tests/unit/uatetime.pas
+++ tests/unit/uatetime.pas
@@ -28,8 +28,7 @@ begin
   assertTrue (true, isLeapDay(2000), 'isleapDay(2000)');
 end;
 
-
-procedure Test_daysInMonth;
+procedure Test_getYYFromIso;
 var
  dateTime : TDateTime;
 
@@ -37,10 +36,95 @@ begin
   dateTime := TDateTime.create;
 
   dateTime.isoDate := '2026-02-01';
-  assertEqual (28, dateTime.getMMFromISO, 'Feb 2026 has 28 days');
+  assertEqual (2026, dateTime.getYYFromISO, 'year 2026');
 
   dateTime.isoDate := '2000-02-01';
-  assertEqual (29, dateTime.getMMFromISO, 'Feb 2020 has 29 days');
+  assertEqual (2000, dateTime.getYYMFromISO, 'year 2000');
+
+  dateTime.Free;
+end;
+
+
+procedure Test_getMMFromIso;
+var
+ dateTime : TDateTime;
+
+begin
+  dateTime := TDateTime.create;
+
+  dateTime.isoDate := '2026-02-01';
+  assertEqual (28, dateTime.getMMFromISO, '28 days in Feb 2026');
+
+  dateTime.isoDate := '2000-12-31';
+  assertEqual (29, dateTime.getMMFromISO, '29 days in Feb 2000');
+
+  dateTime.Free;
+end;
+
+
+procedure Test_getDDFromIso;
+var
+ dateTime : TDateTime;
+
+begin
+  dateTime := TDateTime.create;
+
+  dateTime.isoDate := '2026-02-01';
+  assertEqual (01, dateTime.getDDFromISO, '1st of Feb 2026');
+
+  dateTime.isoDate := '2000-02-01';
+  assertEqual (31, dateTime.getDDFromISO, '31st Dec 2000');
+
+  dateTime.Free;
+end;
+
+
+procedure Test_getHrFromIso;
+var
+ dateTime : TDateTime;
+
+begin
+  dateTime := TDateTime.create;
+
+  dateTime.isoTime := '16:45:23';
+  assertEqual (16, dateTime.getHrFromIso, 'Time = ' + dateTime.isoTime);
+
+  dateTime.isoDate := '01:02:03';
+  assertEqual (1, dateTime.getHrFromIso, 'Time = ' + datetime.isoTime);
+
+  dateTime.Free;
+end;
+
+
+procedure Test_getMinFromIso;
+var
+ dateTime : TDateTime;
+
+begin
+  dateTime := TDateTime.create;
+
+  dateTime.isoTime := '16:45:23';
+  assertEqual (45, dateTime.getMinFromIso, 'Time = ' + dateTime.isoTime);
+
+  dateTime.isoDate := '01:02:03';
+  assertEqual (2, dateTime.getMinFromIso, 'Time = ' + datetime.isoTime);
+
+  dateTime.Free;
+end;
+
+
+procedure Test_getSecFromIso;
+var
+ dateTime : TDateTime;
+
+begin
+  dateTime := TDateTime.create;
+
+  dateTime.isoTime := '16:45:23';
+  assertEqual (23, dateTime.getSecFromIso, 'Time = ' + dateTime.isoTime);
+
+  dateTime.isoDate := '01:02:03';
+  assertEqual (3, dateTime.getSecFromIso, 'Time = ' + datetime.isoTime);
 
   dateTime.Free;
 end;
