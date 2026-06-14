36b08f98097ea9e9dc1f64a6e4306847411508ee
ps68060 <pslegg@scubadivers.co.uk>
2026-06-14 13:39:18 +0100

    Fixes to Timezone additions.

---
+++ src/strnghlp.pas
@@ -0,0 +1,38 @@
+{$I projopts.i}
+{$mode objfpc}{$H+}
+
+unit strnghlp;
+
+(* AUTHOR  : P SLEGG
+   DATE    : 14th June 2026 Version 1
+   PURPOSE : TStringHelper.
+*)
+
+
+interface
+
+type
+  TStringArray = array of String;
+  
+  function Contains(const data : array of ShortString; const S: String)
+          : Boolean;
+
+
+implementation
+
+function Contains(const data : array of ShortString; const S: String)
+        : Boolean;
+  var
+    i        : Integer;
+
+  begin
+    for i := 0 to High(data)
+    do
+      if data[i] = S
+      then
+        Exit(True);
+    
+    Result := False;
+  end;
+
+end.
